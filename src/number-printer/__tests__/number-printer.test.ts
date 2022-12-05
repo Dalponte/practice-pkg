02fc5f17aa8af33a4dfcfc291d9bb10ea8ef2098
DalPonte <maicodalponte@gmail.com>
2022-12-05 20:47:16 -0300

    test(NumberPrinter): add test case for 500 numbers

--- src/number-printer/__tests__/number-printer.test.ts
+++ src/number-printer/__tests__/number-printer.test.ts
@@ -22,5 +22,16 @@ describe("Number printer module", () => {
     expect(log).toHaveBeenNthCalledWith(3, "Visual");
     expect(log).toHaveBeenNthCalledWith(5, "Nuts");
     expect(log).toHaveBeenNthCalledWith(15, "Visual Nuts");
+    expect(log).toHaveBeenNthCalledWith(30, "Visual Nuts");
+  });
+
+  it("should print numbers up to 500", () => {
+    printer.integerPrinter(500);
+    expect(log).toBeCalledTimes(500);
+    expect(log).toHaveBeenNthCalledWith(3, "Visual");
+    expect(log).toHaveBeenNthCalledWith(5, "Nuts");
+    expect(log).toHaveBeenNthCalledWith(127, "127");
+    expect(log).toHaveBeenNthCalledWith(150, "Visual Nuts");
+    expect(log).toHaveBeenNthCalledWith(300, "Visual Nuts");
   });
 });
