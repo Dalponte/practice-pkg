6213daec78464e9d2182e011fb4e656b7661597e
DalPonte <maicodalponte@gmail.com>
2022-12-05 23:16:40 -0300

    feat(LanguageHelper): add function names and basic type

---
+++ src/language-helper/language-helper.ts
@@ -0,0 +1,24 @@
+type CountryLanguageFilter = (languages: string[]) => boolean;
+
+const countCountries = () => {
+  return 0;
+};
+
+const mostLanguagesCountry = (filter?: CountryLanguageFilter) => {
+  return [];
+};
+
+const countLanguages = (countries: string[]) => {
+  return {};
+};
+
+const mostCommonLanguage = () => {
+  return [];
+};
+
+export default {
+  countCountries,
+  mostLanguagesCountry,
+  countLanguages,
+  mostCommonLanguage,
+};
